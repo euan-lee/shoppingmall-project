e2635b395c2d7ea4c90f3c6db7aa80eb096c93bd
euan-lee <maplestory1419@naver.com>
2024-08-14 00:43:39 +0900

    orderPayment  catch boundary 추가

--- cart-project/src/Pages/OrderPayment/OrderPayment.tsx
+++ cart-project/src/Pages/OrderPayment/OrderPayment.tsx
@@ -1,10 +1,20 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { OrderPaymentQueryOptions } from "../../Api/QueryOptions/QueryOptions";
 import { OrderPaymentView } from "./OrderPaymentView";
+import { CatchBoundary } from "@tanstack/react-router";
 
 export const OrderPayment = () => {
   const {
     data: { orders },
   } = useSuspenseQuery(OrderPaymentQueryOptions());
-  return <OrderPaymentView orderDetails={orders} />;
+  return (
+    <>
+      <CatchBoundary
+        getResetKey={() => "reset"}
+        onCatch={(error) => console.error(error)}>
+        <OrderPaymentView orderDetails={orders} />
+      </CatchBoundary>
+      ;
+    </>
+  );
 };
